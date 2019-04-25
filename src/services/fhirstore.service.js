6db00dddbdb93b41fd3963cc9db0c76dbc675722
Lazola Sifuba <sifubalazola@gmail.com>
2019-04-25 09:46:42 +0200

    ANE-35 Add fhir store and PapidPro service placeholders

---
+++ src/services/fhirstore.service.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const addCommunicationRequest = resource => new Promise((resolve, reject) => resolve())
+
+const addCommunicationResource = resource => new Promise((resolve, reject) => resolve())
+
+export default {
+  addCommunicationRequest,
+  addCommunicationResource
+}
