ac15a1a9539c8e6777ecbac0204ab65b70bfa336
Lazola Sifuba <sifubalazola@gmail.com>
2019-04-25 12:40:21 +0200

    ANE-38 Add config values to docker-compose file

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const config = {
-  hearthUrl: process.env.HEARTH_URL || 'http://hearth:3447/',
+  hearthUrl: process.env.HEARTH_URL || 'http://localhost:3447/',
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || '3000'
 }
