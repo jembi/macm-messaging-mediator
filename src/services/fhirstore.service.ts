dfbcca76e6214e83bebbc25a2fc5311b276ead02
Lazola Sifuba <sifubalazola@gmail.com>
2019-05-09 10:53:48 +0200

    Refactor addCommunicationRequest()
    This commit refactors the addCommunicationRequest function to return a
    promise object properly format the error response. This refactor is
    necessary for raising consisting errors for the error handling
    middlware.

--- src/services/fhirstore.service.ts
+++ src/services/fhirstore.service.ts
@@ -5,19 +5,21 @@ import { buildHearthUrl, getResourceIdFromLocationHeader } from '../utils';
 import  { PortNumber } from '../utils/types';
 import { fhirResources, EnvKeys } from '../constants';
 
-const addCommunicationRequest = async (resource: Object) => {
-  const hearthBaseUrl = buildHearthUrl({
-    host: config.get(EnvKeys.HearthHost) as string,
-    port: config.get(EnvKeys.HearthPort) as PortNumber,
-    secured: config.get(EnvKeys.HearthSecured) as boolean,
-    path: `fhir/${fhirResources.COMMUNICATION_REQUEST}`
+const addCommunicationRequest = async (resource: Object): Promise<any> =>
+  new Promise((resolve, reject) => {
+    axios.post(
+      buildHearthUrl({
+        host: config.get(EnvKeys.HearthHost) as string,
+        port: config.get(EnvKeys.HearthPort) as PortNumber,
+        secured: config.get(EnvKeys.HearthSecured) as boolean,
+        path: `fhir/${fhirResources.COMMUNICATION_REQUEST}`
+      }),
+      resource)
+      .then(response => resolve({
+        communicationRequestReference: getResourceIdFromLocationHeader(response.headers.location)
+      }))
+      .catch(err => reject(err.message));
   });
-
-  const response = await axios.post(hearthBaseUrl, resource);
-  return {
-    communicationRequestReference: getResourceIdFromLocationHeader(response.headers.location)
-  };
-};
 
 const addCommunicationResource = (resource: Object) =>
   new Promise((resolve, reject) => resolve(resource));
