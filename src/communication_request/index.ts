3b77e1c86df9509eb147bb4ad9e0f982dc3efec7
Lazola Sifuba <sifubalazola@gmail.com>
2019-06-18 13:11:08 +0200

    IANE-73 Implement search endpoint for CommunicationRequest

--- src/communication_request/index.ts
+++ src/communication_request/index.ts
@@ -1,11 +1,13 @@
 'use strict';
 import express from 'express';
-import { addCommunicationRequest } from './api.handlers';
+import { addCommunicationRequest, getCommunicationRequests } from './api.handlers';
 import { wrapHandler } from '../utils';
 
 const router = express.Router();
 
-router.post('/', wrapHandler(addCommunicationRequest));
+router.route('/')
+  .post(wrapHandler(addCommunicationRequest))
+  .get(wrapHandler(getCommunicationRequests));
 
 export default {
   apiRouter: router
